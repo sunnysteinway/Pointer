6395cf61c5cf4237d12fa43b86ac07a9815fe794
sunnysteinway <76795691+sunnysteinway@users.noreply.github.com>
2021-02-08 09:06:09 +0800

    Update findTheLargestElementInAnArray.cpp

--- findTheLargestElementInAnArray.cpp
+++ findTheLargestElementInAnArray.cpp
@@ -2,33 +2,38 @@
 #include <stdio.h>
 
 int main() {
-    
-    float arr[100];
+
     int n = 0;
-    float* ptr = &arr[0];
-    
+    float* ptr = nullptr;
+
     printf("Please enter the number of elements: ");
-    scanf("%d", &n);
-    
-    for(int i = 0; i < n; i++){
-        
-        printf("Please enter #%d: ", i + 1);
-        scanf("%f", ptr + i);
-        
+    scanf_s("%d", &n);
+
+    ptr = (float*)calloc(n, sizeof(float));
+
+    for (int i = 0; i < n; i++) {
+
+        printf("Please enter #%d: ", i);
+        scanf_s("%f", ptr + i);
+
     }
+
     printf("***\n");
-    for (int k = 0; k < n; k++){
-        printf("Address for #%d element: %p\n", k + 1, ptr + k);
-        printf("Value of #%d element: %f\n", k + 1, *(ptr + k));
+
+    for (int k = 0; k < n; k++) {
+        printf("Address for #%d element: %p\n", k, ptr + k);
+        printf("Value of #%d element: %f\n", k, *(ptr + k));
+        printf("\n");
     }
-    for (int j = 1; j < n; j++) {
-        
-        if (*(ptr + j) > *ptr){
-            
+
+    for (int j = 0; j < n; j++) {
+
+        if (*(ptr + j) > *ptr) {
+
             ptr = (ptr + j);
-            
+
         }
-        
+
     }
     printf("***\n");
     printf("Largest element's address: %p\n", ptr);
